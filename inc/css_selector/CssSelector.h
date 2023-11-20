47fa273621b22e72e8abb3bad70d123d148f7033
Florentino Ding <33351272+Florentino-Ding@users.noreply.github.com>
2023-11-20 19:56:30 +0800

    ✨ feat(class interface, class html_parser): add text_only mode
    add text_only mode to output only text, refactor class interface

---
+++ inc/css_selector/CssSelector.h
@@ -0,0 +1,16 @@
+//
+// Created by 丁麓然 on 11/20/23.
+//
+
+#ifndef HTMLPARSER_CSSSELECTOR_H
+#define HTMLPARSER_CSSSELECTOR_H
+
+
+
+class CssSelector {
+
+};
+
+
+
+#endif //HTMLPARSER_CSSSELECTOR_H
---
+++ src/css_selector/CssSelector.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 丁麓然 on 11/20/23.
+//
+
+#include "CssSelector.h"
